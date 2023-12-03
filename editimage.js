a78482cc02ffc8906d4a2999d8470d43ba6b5fc8
D Karthik reddy <124157018@sastra.ac.in>
2023-12-03 16:33:30 +0530

    update

--- editimage.js
+++ editimage.js
@@ -4,5 +4,26 @@ document.addEventListener('DOMContentLoaded', function () {
     const urlParams = new URLSearchParams(window.location.search);
     const option = urlParams.get('option');
     document.getElementById('optionText').innerText = `Option: ${option}`;
-  });
-  
+
+    const imageInput = document.getElementById('imageInput');
+    const submitBtn = document.getElementById('submitBtn');
+    const canvas = document.getElementById('imageCanvas');
+    const context = canvas.getContext('2d');
+
+    submitBtn.addEventListener('click', function () {
+        const file = imageInput.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = function (e) {
+                const img = new Image();
+                img.onload = function () {
+                    canvas.width = img.width;
+                    canvas.height = img.height;
+                    context.drawImage(img, 0, 0);
+                };
+                img.src = e.target.result;
+            };
+            reader.readAsDataURL(file);
+        }
+    });
+});
